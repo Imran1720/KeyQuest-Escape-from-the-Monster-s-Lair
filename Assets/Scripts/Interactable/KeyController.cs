5eb85a1fb737fe2191ded63dfc765b0c1744d796
Imran1720 <skimran1720@gmail.com>
2024-12-28 15:15:12 +0530

    Added Animation to Key gameObject

--- Assets/Scripts/Interactable/KeyController.cs
+++ Assets/Scripts/Interactable/KeyController.cs
@@ -4,6 +4,14 @@ public class KeyController : MonoBehaviour
 {
 
     public int keyCount;
+    int facingDirection = 1;
+    public float moveSpeed;
+
+    Vector3 startpos;
+    private void Start()
+    {
+        startpos = transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
@@ -13,4 +21,26 @@ public class KeyController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
+    private void Update()
+    {
+
+        transform.Translate(new Vector3(0, facingDirection * moveSpeed * Time.deltaTime, 0));
+
+        FlipDirection();
+    }
+
+    void FlipDirection()
+    {
+        if (transform.position.y > startpos.y + .3f)
+        {
+            facingDirection = -1;
+        }
+        else if (transform.position.y < startpos.y)
+        {
+            facingDirection = 1;
+        }
+    }
+
 }
